8f8945f4ce3356293d0fa89d04ca113bb34adc06
Zohaib <101661680+zabi1647@users.noreply.github.com>
2024-11-19 19:04:29 +0500

    moving faculty page

--- lib/screens/faculty/faculity_landing_page.dart
+++ lib/screens/faculty/faculity_landing_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:maju_trackmate/controller/student_landing_page_controller.dart';
